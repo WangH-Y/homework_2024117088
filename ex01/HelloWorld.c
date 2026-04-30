09ad1640fbffafbf52f58b8aa2107746ada8a885
王不正 <2751960030@qq.com>
2026-04-30 20:19:36 +0800

    恢复文件并同步远程

---
+++ ex01/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+
+    printf("请输入文字：");
+    scanf("%s", a);
+
+    printf("你输入的文字是：%s!\n", a);
+    printf("Hello,World!\n");
+
+    return 0;
+}
